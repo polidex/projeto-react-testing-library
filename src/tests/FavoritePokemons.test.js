31582cc93958d1117b9cfdd64213d2089eb91902
polidex <lp.igor@gmail.com>
2022-06-07 18:49:52 -0300

    request 3 done

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente FavoritePokemons', () => {
+  test('Se aparece mensagem No favorite pokemon found, se não tem pokémons favoritados',
+    () => {
+      renderWithRouter(<FavoritePokemons />);
+      const noFvrtPokemon = screen.getByText(/No favorite pokemon found/i);
+      expect(noFvrtPokemon).toBeInTheDocument();
+    });
+  test('Teste se são exibidos todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const fvrtPokemons = document.getElementsByClassName('favorite-pokemon');
+    expect(fvrtPokemons).toHaveLength(0);
+  });
+});
