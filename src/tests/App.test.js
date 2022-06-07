866a5cf9e239d41c395bed703c73e769b6d6e762
polidex <lp.igor@gmail.com>
2022-06-07 17:32:50 -0300

    req 1 lint fix

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,4 @@
 import { screen } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
 import React from 'react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
