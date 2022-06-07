bdf7904d637a3761c1ddb997dbbd5eef84d29a59
polidex <lp.igor@gmail.com>
2022-06-07 20:05:10 -0300

    request 4 done

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente NotFound', () => {
+  test('Se a página contém um heading h2 com o texto Page requested not found 😭', () => {
+    renderWithRouter(<NotFound />);
+    const pageNotFound = screen.getByRole('heading',
+      { name: /Page requested not found/i, level: 2 });
+    expect(pageNotFound).toBeInTheDocument();
+  });
+  test('Se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+    const imgNotFound = screen
+      .getByAltText(/Pikachu crying because the page requested was not found/i);
+    expect(imgNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
